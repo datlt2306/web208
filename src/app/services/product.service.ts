547cc3f2cb29026dd58dc759f9ee3b02d83f2b00
Le Trong Dat <datlt2306@gmail.com>
2022-05-26 07:51:52 +0700

    feat: delete item

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -7,19 +7,21 @@ import { IProduct } from '../models/Product';
   providedIn: 'root'
 })
 export class ProductService {
+  API_URL = `http://localhost:3000/products/`
+
   constructor(
     private http: HttpClient
   ) { }
 
   getProduct(id: any): Observable<IProduct> {
     // return data.find(item => item.id === id);
-    return this.http.get<IProduct>(`http://localhost:3000/products/${id}`);
+    return this.http.get<IProduct>(`${this.API_URL}/${id}`);
   }
   getProductList(): Observable<IProduct[]> {
-    return this.http.get<IProduct[]>(`http://localhost:3000/products`);
+    return this.http.get<IProduct[]>(`${this.API_URL}`);
   }
-  removeProduct() {
-
+  removeProduct(id: number): Observable<IProduct> {
+    return this.http.delete<IProduct>(`${this.API_URL}/${id}`);
   }
   addProduct() {
 

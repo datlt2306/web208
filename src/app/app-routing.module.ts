d16f8cf74fe338138ab66018cff23aa391ea2438
Le Trong Dat <datlt2306@gmail.com>
2022-09-26 15:34:11 +0700

    guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './auth.guard';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { DetailProductComponent } from './pages/detail-product/detail-product.component';
 import { HomeComponent } from './pages/home/home.component';
@@ -8,10 +9,10 @@ import { ProductsComponent } from './pages/products/products.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/abc', pathMatch: 'full' },
-  { path: 'abc', component: HomeComponent },
+  { path: 'abc', component: HomeComponent, canActivate: [AuthGuard] },
   {
-    path: 'products', component: ProductsComponent, children: [
-      { path: "", component: ProductListComponent },
+    path: 'products', component: ProductsComponent, canActivate: [AuthGuard], children: [
+      { path: "list", component: ProductListComponent },
     ]
   },
   {

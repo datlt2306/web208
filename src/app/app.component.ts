e0900a3d9428f08b2212d8cd8704a1741cc9614a
Le Trong Dat <datlt2306@gmail.com>
2023-05-08 14:59:26 +0700

    Buổi 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
-
+// decorators
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Angular';
+  title: string = 'Angular';
+  myName: string = "Le Trong Dat";
+  myStatus: boolean = false
 }
+
+
+
+// function Component(){
+//   return <div>1</div>
+// }
+// <Component />

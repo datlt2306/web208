1c37444ec5cf59e3427661cea0aad6168021feeb
Le Trong Dat <datlt2306@gmail.com>
2023-05-09 11:01:17 +0700

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular';
+  myName: string = 'Derek';
+  myAge: number = 30;
+  myStatus: boolean = true;
+  myInfo: { name: string, age: number } = {
+    name: "Derek",
+    age: 30
+  };
+  showInfo() {
+    return this.myAge + 10;
+  }
 }
+
+// Logic, View, Stylesheet
+// .ts, .html, .scss
+
+
+// function HelloComponent(){
+
+// }
+
+// <HelloComponent />

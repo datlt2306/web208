a9ee96cc7e1e51b2595e4bcafb28320ab4f30181
Le Trong Dat <datlt2306@gmail.com>
2023-05-09 15:43:00 +0700

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root', // định nghĩa element
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'Angular';
+  myName: string = "Le Trong Dat";
+  myAge: number = 20;
+  myStatus: boolean = true;
+  myInfo: { name: string, age: number } = {
+    name: "Le Trong Dat",
+    age: 20
+  };
+
+  showInfo(name: string) {
+    return `My name is ${name ? name : this.myName} and I'm ${this.myAge} years old`;
+  }
 }

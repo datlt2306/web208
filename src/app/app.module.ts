9c66faeab5e00d97c8bee50fabc8d0d4bc16f053
Le Trong Dat <datlt2306@gmail.com>
2023-05-10 14:49:57 +0700

    Buổi 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HelloComponent } from './components/HelloComponent/Hello.component';
-import { Hello2Component } from './components/hello2/hello2.component';
 import { BrowserModule } from '@angular/platform-browser';
+import { ProductListComponent } from './components/product-list/product-list.component';
 
 // decorators
 @NgModule({
   declarations: [
     AppComponent,
-    HelloComponent,
-    Hello2Component
+    ProductListComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/product-list/product-list.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { IProduct } from 'src/app/interfaces/Product';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.scss']
+})
+export class ProductListComponent {
+  products: IProduct[] = [
+    { _id: 1, name: 'Product 1', price: 100, img: "https://picsum.photos/200/200" },
+    { _id: 2, name: 'Product 2', price: 200, img: "https://picsum.photos/200/200" },
+    { _id: 3, name: 'Product 3', price: 300, img: "https://picsum.photos/200/200" },
+  ]
+}

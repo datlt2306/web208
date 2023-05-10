b73f701af3c43ee1b1a3900356afea1b23b79d4c
Le Trong Dat <datlt2306@gmail.com>
2023-05-10 13:35:34 +0700

    directive

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -12,4 +12,14 @@ export class ProductListComponent {
     { _id: 2, name: "Product B", price: 300, img: "https://picsum.photos/200/200" },
     { _id: 3, name: "Product C", price: 400, img: "https://picsum.photos/200/200" },
   ]
+
+  status: boolean = false;
+  productName = ""
+
+  getValue(e: any) {
+    this.productName = e.target.value;
+  }
+  changeStatus() {
+    this.status = !this.status;
+  }
 }

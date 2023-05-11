6b990d83f0ce51eb7deae710f9d36f1170d63a04
Le Trong Dat <datlt2306@gmail.com>
2023-05-11 15:09:59 +0700

    Buổi 2

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IProduct } from 'src/app/interfaces/Product';
 
 @Component({
     selector: 'app-product-list',
@@ -8,12 +9,20 @@ import { Component } from '@angular/core';
 export class ProductListComponent {
     title = "Quản lý sản phẩm";
     status: boolean = false;
-
+    products: IProduct[] = [
+        { _id: 1, name: "Sản phẩm A", price: 200, img: "Ảnh" },
+        { _id: 2, name: "Sản phẩm B", price: 300, img: "Ảnh" }
+    ]
     handleClick() {
         this.status = !this.status;
     }
-
     setValue(e: any) {
         this.title = e.target.value
+    }
+    removeItem(id: any) {
+        const confirm = window.confirm('Mày chắc chắn muốn xóa chứ?');
+        if (confirm) {
+            this.products = this.products.filter(item => item._id !== id);
+        }
     }
 }

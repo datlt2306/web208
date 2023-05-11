37247b75b0d3aee53fa77745698c0debb6e9181a
Le Trong Dat <datlt2306@gmail.com>
2023-05-11 10:08:08 +0700

    Buổi 2

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IProduct } from 'src/app/interfaces/Product';
 
 @Component({
   selector: 'app-product-list',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./product-list.component.scss']
 })
 export class ProductListComponent {
-
+  title = 'Quản lý sản phẩm';
+  products: IProduct[] = [
+    { _id: 1, name: "Sản phẩm A", price: 1000, img: "https://picsum.photos/200/200" },
+    { _id: 2, name: "Sản phẩm B", price: 2000, img: "https://picsum.photos/200/200" },
+  ]
 }

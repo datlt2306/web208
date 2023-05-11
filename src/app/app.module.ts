59df4a60c3e1ff4d15984e2ae3a702f18a310476
Le Trong Dat <datlt2306@gmail.com>
2023-05-11 13:49:07 +0700

    Buổi 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
+import { HomeComponent } from './home/home.component';
 
 // decorators
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,

a0ce600b40c27c4dbb2fdd3fffded62bbba69926
Le Trong Dat <datlt2306@gmail.com>
2023-05-17 15:40:30 +0700

    Buổi 5: Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,28 @@ import { HomePageComponent } from './pages/home-page/home-page.component';
 import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
 import { BaseLayoutComponent } from './layouts/base-layout/base-layout.component';
+import { AboutComponent } from './pages/about/about.component';
+import { DashboardComponent } from './pages/admin/dashboard/dashboard.component';
+import { AdminProductComponent } from './pages/admin/admin-product/admin-product.component';
 
 const routes: Routes = [
-  { path: '', component: BaseLayoutComponent },
-  { path: 'admin', component: AdminLayoutComponent },
+  {
+    path: '', component: BaseLayoutComponent, children: [
+      { path: '', component: HomePageComponent },
+      { path: 'about', component: AboutComponent },
+    ]
+  },
+  {
+    path: 'admin', component: AdminLayoutComponent, children: [
+      { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+      { path: 'dashboard', component: DashboardComponent },
+      { path: 'product', component: AdminProductComponent }
+    ]
+  },
   { path: '**', component: PageNotFoundComponent }
 ];
+
+// admin/dashboard
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

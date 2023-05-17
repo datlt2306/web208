321fb213b8abd55d8e5fb9c2d5f857fd2f86bee2
Le Trong Dat <datlt2306@gmail.com>
2023-05-17 15:18:20 +0700

    Buổi 5: routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+import { BaseLayoutComponent } from './layouts/base-layout/base-layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: BaseLayoutComponent },
+  { path: 'admin', component: AdminLayoutComponent },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,26 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { ProductListComponent } from './components/product-list/product-list.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { AboutComponent } from './pages/about/about.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { DashboardComponent } from './pages/admin/dashboard/dashboard.component';
+import { AdminProductComponent } from './pages/admin/admin-product/admin-product.component';
+import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+import { BaseLayoutComponent } from './layouts/base-layout/base-layout.component';
 
 // decorators
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
+    HomePageComponent,
+    AboutComponent,
+    PageNotFoundComponent,
+    DashboardComponent,
+    AdminProductComponent,
+    AdminLayoutComponent,
+    BaseLayoutComponent,
   ],
   imports: [
     BrowserModule,

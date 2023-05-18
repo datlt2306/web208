2211ae1f515c1365966c5227e175cc36da6db5ae
Le Trong Dat <datlt2306@gmail.com>
2023-05-18 12:49:44 +0700

    Buổi 5: routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,28 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { HomeComponent } from './home/home.component';
+import { BaseLayoutComponent } from './layouts/base-layout/base-layout.component';
+import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+import { AboutPageComponent } from './pages/about-page/about-page.component';
+import { DashboardComponent } from './pages/admin/dashboard/dashboard.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { AdminProductComponent } from './pages/admin/admin-product/admin-product.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'products', component: ProductListComponent }
+  {
+    path: '', component: BaseLayoutComponent, children: [
+      { path: "", component: HomeComponent },
+      { path: "about", component: AboutPageComponent }
+    ]
+  },
+  {
+    path: 'admin', component: AdminLayoutComponent, children: [
+      { path: "", redirectTo: "dashboard", pathMatch: "full" },
+      { path: "dashboard", component: DashboardComponent },
+      { path: "product", component: AdminProductComponent }
+    ]
+  },
+  { path: "**", component: PageNotFoundComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { HomeComponent } from './home/home.component';
+import { BaseLayoutComponent } from './layouts/base-layout/base-layout.component';
+import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+import { AboutPageComponent } from './pages/about-page/about-page.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { DashboardComponent } from './pages/admin/dashboard/dashboard.component';
+import { AdminProductComponent } from './pages/admin/admin-product/admin-product.component';
 
 // decorators
 @NgModule({
@@ -12,6 +19,13 @@ import { HomeComponent } from './home/home.component';
     AppComponent,
     ProductListComponent,
     HomeComponent,
+    BaseLayoutComponent,
+    AdminLayoutComponent,
+    AboutPageComponent,
+    PageNotFoundComponent,
+    HomePageComponent,
+    DashboardComponent,
+    AdminProductComponent,
   ],
   imports: [
     BrowserModule,

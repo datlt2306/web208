919f4bd9ede0f8888835850af271708d0420ff93
Le Trong Dat <datlt2306@gmail.com>
2023-05-20 14:59:47 +0700

    Buổi 6: Edit

--- src/app/pages/admin/product-edit/product-edit.component.ts
+++ src/app/pages/admin/product-edit/product-edit.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { IProduct } from 'src/app/interfaces/Product';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-product-edit',
@@ -6,5 +9,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./product-edit.component.scss']
 })
 export class ProductEditComponent {
+  product: IProduct = {
+    name: "",
+    price: 0
+  }
+  constructor(
+    private productService: ProductService,
+    private route: ActivatedRoute) {
 
+    // Observable
+    this.route.paramMap.subscribe(param => {
+      const id = Number(param.get('id'));
+      this.productService.getProductById(id).subscribe(product => {
+        this.product = product;
+      })
+    })
+  }
+  onHandleSubmit() {
+    this.productService.updateProduct(this.product).subscribe(data => {
+      console.log(data)
+    })
+  }
 }

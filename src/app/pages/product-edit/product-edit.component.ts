b159f5f27a62014e30a5b430a14d354986f01295
Le Trong Dat <datlt2306@gmail.com>
2023-05-22 14:51:28 +0700

    Buổi 7: update

--- src/app/pages/product-edit/product-edit.component.ts
+++ src/app/pages/product-edit/product-edit.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { IProduct } from 'src/app/interfaces/Product';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-product-edit',
@@ -6,5 +9,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./product-edit.component.scss']
 })
 export class ProductEditComponent {
-
+  product: IProduct = {
+    name: "",
+    price: 0
+  }
+  constructor(private router: ActivatedRoute, private productService: ProductService,) {
+    this.router.paramMap.subscribe((params => {
+      const id = Number(params.get('id'));
+      this.productService.getProduct(id).subscribe(data => {
+        this.product = data;
+      }, error => console.log(error.message))
+    }))
+  }
+  onHandleSubmit() {
+    this.productService.updateProduct(this.product).subscribe((product) => {
+      console.log('product', product);
+    })
+  }
 }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -22,4 +22,7 @@ export class ProductService {
   addProduct(product: IProduct): Observable<IProduct> {
     return this.http.post<IProduct>(`http://localhost:3000/products`, product)
   }
+  updateProduct(product: IProduct): Observable<IProduct> {
+    return this.http.patch<IProduct>(`http://localhost:3000/products/${product.id}`, product)
+  }
 }

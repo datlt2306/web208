fa53629dfcc8d9ae31c091ba8330cf3f517bd511
Le Trong Dat <datlt2306@gmail.com>
2023-05-24 16:46:34 +0700

    Buổi 7: reactive form

--- src/app/pages/product-add/product-add.component.ts
+++ src/app/pages/product-add/product-add.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IProduct } from 'src/app/interfaces/Product';
 import { ProductService } from 'src/app/services/product.service';
+import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-product-add',
@@ -8,18 +9,29 @@ import { ProductService } from 'src/app/services/product.service';
   styleUrls: ['./product-add.component.scss']
 })
 export class ProductAddComponent {
-  product: IProduct = {
-    name: "",
-    price: 0,
-    img: ""
-  }
-  constructor(private productService: ProductService) {
+  // product: IProduct = {
+  //   name: "",
+  //   price: 0,
+  //   img: ""
+  // }
+  // constructor(private productService: ProductService) {
 
-  }
+  // }
 
-  onHandleAdd() {
-    this.productService.addProduct(this.product).subscribe(product => {
-      console.log(product)
-    })
+  // onHandleAdd() {
+  //   this.productService.addProduct(this.product).subscribe(product => {
+  //     console.log(product)
+  //   })
+  // }
+  productForm = this.formBuilder.group({
+    name: [''],
+    price: [0],
+  })
+  constructor(
+    private productService: ProductService,
+    private formBuilder: FormBuilder) { }
+
+  onHandleSubmit() {
+    console.log(this.productForm.value)
   }
 }

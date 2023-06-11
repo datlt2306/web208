aa636cc059a9080e14c4ec4c9c5400f87ba1b95b
Le Trong Dat <datlt2306@gmail.com>
2023-06-11 13:29:24 +0700

    Buổi 15: Product Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './auth.guard';
-import { ProductListComponent } from './components/product-list/product-list.component';
 import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
 import { BaseLayoutComponent } from './layouts/base-layout/base-layout.component';
+import { ProductRoutingModule } from './modules/product/product-routing.module';
 import { AboutComponent } from './pages/about/about.component';
 import { DashboardComponent } from './pages/admin/dashboard/dashboard.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
-import { ProductAddComponent } from './pages/product-add/product-add.component';
 import { ProductDetailComponent } from './pages/product-detail/product-detail.component';
-import { ProductEditComponent } from './pages/product-edit/product-edit.component';
 import { SigninComponent } from './pages/signin/signin.component';
 import { SignupComponent } from './pages/signup/signup.component';
 
@@ -28,9 +26,7 @@ const routes: Routes = [
     path: 'admin', component: AdminLayoutComponent, canActivate: [AuthGuard], children: [
       { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
       { path: 'dashboard', component: DashboardComponent },
-      { path: 'product', component: ProductListComponent },
-      { path: 'product/add', component: ProductAddComponent },
-      { path: 'product/:id/edit', component: ProductEditComponent }
+      ...ProductRoutingModule.routes
     ]
   },
   { path: '**', component: PageNotFoundComponent }
--- src/app/modules/product/product-routing.module.ts
+++ src/app/modules/product/product-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductListComponent } from 'src/app/components/product-list/product-list.component';
+import { ProductAddComponent } from 'src/app/pages/product-add/product-add.component';
+import { ProductEditComponent } from 'src/app/pages/product-edit/product-edit.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'product', component: ProductListComponent },
+  { path: 'product/add', component: ProductAddComponent },
+  { path: 'product/:id/edit', component: ProductEditComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class ProductRoutingModule { }
+export class ProductRoutingModule {
+  static routes = routes;
+}

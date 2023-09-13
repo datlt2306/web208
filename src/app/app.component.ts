3f7ffd19a94bd34e471b7c4d2fecb1a28327629a
Le Trong Dat <datlt2306@gmail.com>
2023-09-13 15:32:20 +0700

    Buổi 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'sum22';
+  myName = "Dat";
 }
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,ts}", "./src/app/**/*.{html,ts}"],
+  prefix: "tw-",
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};

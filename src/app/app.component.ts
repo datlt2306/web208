4a0c06f3bb93a657af342d9035d076ea7bfd91c4
Le Trong Dat <datlt2306@gmail.com>
2023-09-14 13:05:40 +0700

    Buổi 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,30 @@
 import { Component } from '@angular/core';
+import { IStudent } from './interfaces/student';
 
 @Component({
+  // Định nghĩa 1 element cho component
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'sum22';
-  myName: string = 'Le Trong Dat';
+  student: IStudent = {
+    // key: value
+    name: 'John Doe',
+    age: 20,
+    imageUrl: 'https://picsum.photos/200/300',
+    status: true,
+    address: {
+      street: '123 Main St',
+      city: 'Boston',
+      state: 'MA'
+    }
+  }
 
-  run() {
-    return `Hello ${this.myName}`;
+  showStudentName() {
+    return this.student.name
+  }
+  setName(newName: string) {
+    this.student.name = newName
   }
 }

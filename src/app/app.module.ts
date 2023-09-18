54b3284fe3799e5f7386059b5758451d9f92b6cc
Le Trong Dat <datlt2306@gmail.com>
2023-09-18 14:51:57 +0700

    Buổi 4: Custom Pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { ProductsComponent } from './components/products/products.component';
+import { SearchBoxPipe } from './search-box.pipe';
+import { ChangeNumberPipe } from './change-number.pipe';
 // github.com/datlt2306/web208
 @NgModule({
   declarations: [
     AppComponent,
     ProductsComponent,
+    SearchBoxPipe,
+    ChangeNumberPipe,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/search-box.pipe.ts
@@ -0,0 +1,13 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { IProduct } from './interfaces/product';
+
+@Pipe({
+  name: 'searchBox'
+})
+export class SearchBoxPipe implements PipeTransform {
+
+  transform(data: IProduct[], searchText: string): IProduct[] {
+    return data.filter(item => item.name.toLowerCase().includes(searchText.toLowerCase()));;
+  }
+
+}
